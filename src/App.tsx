819f8d04f8afa441b1a0e638729e511c11a88aee
Omar Medhat <omarmedhat2004@gmail.com>
2025-09-17 14:06:21 +0800

    Updated home screen

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/simulator" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
